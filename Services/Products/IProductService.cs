d7c1d15232536519364f99c9292dd1f62f7350dc
AngelKazakov <drakex167@gmail.com>
2024-05-29 11:28:25 +0300

    Implement getting product by given Id.

---
+++ Services/Products/IProductService.cs
@@ -0,0 +1,9 @@
+﻿using RandomShop.Data.Models;
+
+namespace RandomShop.Services.Products
+{
+    public interface IProductService
+    {
+        public Task<Product> GetProductById(int productId);
+    }
+}
---
+++ Services/Products/ProductService.cs
@@ -0,0 +1,23 @@
+﻿using RandomShop.Data;
+using RandomShop.Data.Models;
+using RandomShop.Models.Product;
+
+namespace RandomShop.Services.Products
+{
+    public class ProductService : IProductService
+    {
+        private readonly ShopContext context;
+
+        public ProductService(ShopContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<Product> GetProductById(int productId)
+        {
+            Product product = await this.context.Products.FindAsync(productId);
+
+            return product ?? throw new NullReferenceException("Product not found.");
+        }
+    }
+}
