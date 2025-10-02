6e8c99aba0c4eb983149324ab341454c344a126c
AngelKazakov <drakex167@gmail.com>
2025-10-02 15:14:19 +0300

    Implement basic ViewCart action and view to display shopping cart items and grand total

---
+++ Controllers/CartController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RandomShop.Data.Models;
+using RandomShop.Infrastructure;
+using RandomShop.Models.Cart;
+using RandomShop.Services.Cart;
+
+namespace RandomShop.Controllers;
+
+public class CartController : Controller
+{
+    private readonly ICartService cartService;
+
+    public CartController(ICartService cartService)
+    {
+        this.cartService = cartService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> ViewCart()
+    {
+        var userId = this.User.Id();
+
+        var cartItems = await this.cartService.GetCartItemsAsync(userId);
+
+        var model = new CartViewModel() { Items = cartItems };
+
+        return View(model);
+    }
+}
---
+++ Models/Cart/CartViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace RandomShop.Models.Cart;
+
+public class CartViewModel
+{
+    public ICollection<CartItemViewModel> Items { get; set; } = new List<CartItemViewModel>();
+
+    public decimal GrandTotal => this.Items.Sum(item => item.TotalPrice);
+}
