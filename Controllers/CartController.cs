df88fd40b938584f175a7428eff634827358b0c3
AngelKazakov <drakex167@gmail.com>
2025-10-06 15:06:37 +0300

    Implement RemoveFromCart functionality with controller action and view button to delete items from cart

--- Controllers/CartController.cs
+++ Controllers/CartController.cs
@@ -20,6 +20,9 @@ public class CartController : Controller
     [Authorize]
     public async Task<IActionResult> ViewCart()
     {
+        //Check if only user is logged in then create a cart.
+        //Now it creates a cart always it doesn't matter if user is logged in...!
+        //Check correct applying promotion in shopping cart. Getting the original one not the discounted.
         var userId = this.User.Id();
 
         var cartItems = await this.cartService.GetCartItemsAsync(userId);
@@ -36,6 +39,17 @@ public class CartController : Controller
         string userId = this.User.Id();
 
         await this.cartService.AddToCart(userId, id);
+
+        return RedirectToAction("ViewCart");
+    }
+
+    [HttpPost]
+    [Authorize]
+    public async Task<IActionResult> Remove(int id)
+    {
+        string userId = this.User.Id();
+
+        await this.cartService.RemoveFromCart(userId, id);
 
         return RedirectToAction("ViewCart");
     }
