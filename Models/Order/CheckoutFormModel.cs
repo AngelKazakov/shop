f45165dee09642fd3ed04ddbdd7bfa21fa3d371b
AngelKazakov <drakex167@gmail.com>
2025-12-03 16:17:27 +0200

    -Prevents form submission if required fields are missing in shipping, payment, or the active address section.
    -Dynamically toggles the required attribute for the saved address dropdown and the new address input fields based on radio button selection.

--- Models/Order/CheckoutFormModel.cs
+++ Models/Order/CheckoutFormModel.cs
@@ -6,39 +6,35 @@ namespace RandomShop.Models.Order;
 
 public class CheckoutFormModel
 {
-    
-    [Required]
-    public int SelectedShippingMethod { get; set; }
-    
-    [Required]
+    [Required] 
+    public int SelectedShippingMethodId { get; set; }
+
+    [Required] 
     public int SelectedAddressId { get; set; }
-    
+
     [Required]
     public int PaymentTypeId { get; set; }
-    
+
     public bool UseNewAddress { get; set; }
-    
-    [Required]
-    public int CountryId { get; set; }
+
+    public int? CountryId { get; set; }
 
     [Required]
     [Range(DataConstants.Address.StreetNumberMin, DataConstants.Address.StreetNumberMax)]
-    public int StreetNumber { get; set; }
+    public int? StreetNumber { get; set; }
 
     [Required]
     [StringLength(DataConstants.Address.addressLineMaxLength)]
-    public string AddressLine1 { get; set; }
+    public string? AddressLine1 { get; set; }
 
     [StringLength(DataConstants.Address.addressLineMaxLength)]
-    public string AddressLine2 { get; set; }
+    public string? AddressLine2 { get; set; }
 
     [Required]
     [Range(DataConstants.Address.postalCodeMin, DataConstants.Address.postalCodeMax)]
-    public int PostalCode { get; set; }
+    public int? PostalCode { get; set; }
 
-   // public string? Provider { get; set; }
+    // public string? Provider { get; set; }
 
-   // public decimal OrderTotal { get; set; }
-    
-
+    // public decimal OrderTotal { get; set; }
 }
